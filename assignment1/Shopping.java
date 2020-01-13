40cd13eaf74d3186691b2c9d4cf73d9857a6c535
gopal gupta <gopal.gupta@metacube.com>
2020-01-13 10:56:21 +0530

    SCF1 assignment

---
+++ assignment1/Shopping.java
@@ -0,0 +1,157 @@
+package assignment1;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Shopping {
+	int ProductPrice[] = new int[5];
+	String ProductName[] = new String[5];
+	ArrayList<bucket> al = new ArrayList<bucket>();
+
+	void setProductName() {
+		ProductName[0] = "Nokia";
+		ProductName[1] = "Samsung";
+		ProductName[2] = "Gionee";
+		ProductName[3] = "Apple";
+		ProductName[4] = "Xiomi";
+	}
+
+	void setProductPrice() {
+		ProductPrice[0] = 2;
+		ProductPrice[1] = 4;
+		ProductPrice[2] = 6;
+		ProductPrice[3] = 8;
+		ProductPrice[4] = 10;
+	}
+
+	int noOfProductInList() {
+		return ProductName.length;
+	}
+
+	void displayListOfProducts() {
+		System.out
+				.println("*********************************************************");
+		System.out.println("SN\tProduct Name\tPrice");
+		for (int i = 0; i < 5; i++)
+			System.out.println(i + 1 + "\t" + ProductName[i] + "\t\t"
+					+ ProductPrice[i]);
+		System.out
+				.println("*********************************************************");
+	}
+
+	int SelectOption(Scanner scanner) {
+		int SelectOption;
+		System.out
+				.println("*********************************************************");
+		System.out.println("1: Add Product in Bucket");
+		System.out.println("2: Remove Product form Bucket");
+		System.out.println("3: Display Orders List");
+		System.out.println("4: Place the Order");
+		System.out.print("Your Option: ");
+		SelectOption = scanner.nextInt();
+		System.out
+				.println("*********************************************************");
+		return SelectOption;
+	}
+
+	void AddInCart(int ProductSN, int Quantity) {
+		System.out.println(Quantity + " Peace Of " + ProductName[ProductSN - 1]
+				+ " SmartPhone is Successfully Added");
+		updateList(ProductSN, Quantity);
+	}
+
+	boolean isListEmpty() {
+		return al.isEmpty();
+	}
+
+	int sizeOfList() {
+		return al.size();
+	}
+
+	void removeFromList(int RemoveProductSN) {
+		al.remove(RemoveProductSN - 1);
+	}
+
+	int totalPrice() {
+		int TotalPrice = 0;
+		Iterator itr = al.iterator();
+
+		// traversing elements of ArrayList scannerect
+		while (itr.hasNext()) {
+			bucket st = (bucket) itr.next();
+			TotalPrice = TotalPrice + st.price;
+		}
+		return TotalPrice;
+	}
+
+	int totalProduct() {
+		int TotalProduct = 0;
+		Iterator itr = al.iterator();
+
+		// traversing elements of ArrayList scannerect
+		while (itr.hasNext()) {
+			bucket st = (bucket) itr.next();
+			TotalProduct = TotalProduct + st.qnt;
+		}
+		return TotalProduct;
+	}
+
+	void displayFinalBucketProducts() {
+		Iterator itr = al.iterator();
+		int ProductSequence = 1;
+		System.out
+				.println("*********************************************************");
+		System.out.println("Product Sequence\tProduct Name\tQuantity\tPrice");
+
+		// traversing elements of ArrayList scannerect
+		while (itr.hasNext()) {
+			bucket st = (bucket) itr.next();
+			System.out.println(ProductSequence++ + "\t\t\t" + st.name + "\t\t"
+					+ st.qnt + "\t\t" + st.price);
+		}
+		System.out
+				.println("*********************************************************");
+	}
+
+	void orderPlace() {
+		System.out
+				.println("*********************************************************");
+		System.out.println("Quantity of Products: " + totalProduct()
+				+ "\nTotal Price: " + totalPrice());
+	}
+
+	void updateList(int productSequenceNumber, int Qnt) {
+		Iterator itr = al.iterator();
+		boolean flag = true;
+		// traversing elements of ArrayList scannerect
+		while (itr.hasNext()) {
+			bucket st = (bucket) itr.next();
+			if (st.sn == productSequenceNumber - 1) {
+				st.qnt = st.qnt + Qnt;
+				st.price = st.price
+						+ (Qnt * ProductPrice[productSequenceNumber - 1]);
+				flag = false;
+				break;
+			}
+		}
+		if (flag) {
+			al.add(new bucket(productSequenceNumber - 1,
+					ProductName[productSequenceNumber - 1],
+					ProductPrice[productSequenceNumber - 1] * Qnt, Qnt));
+		}
+	}
+}
+
+class bucket {
+	int sn;
+	String name;
+	int price;
+	int qnt;
+
+	bucket(int sn, String name, int price, int qnt) {
+		this.sn = sn;
+		this.name = name;
+		this.price = price;
+		this.qnt = qnt;
+	}
+}
---
+++ assignment1/shop.java
@@ -0,0 +1,134 @@
+package assignment1;
+
+import java.util.Scanner;
+
+public class shop {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		User newUser = new User();
+		// newUser.authenticate(scanner);
+		int Option;
+		Shopping item = new Shopping();
+		item.setProductName();
+		item.setProductPrice();
+
+		item.displayListOfProducts();
+		while (true) {
+			Option = item.SelectOption(scanner);
+			switch (Option) {
+
+			// Case 1 to Add New Item in Our Bucket
+			case 1:
+				item.displayListOfProducts();
+				int sequenceNumberOfProduct,
+				quantityOfProduct;
+				int indexOfFirstProducts = 0;
+				int maxProductQuantity = 100;
+				int minProductQuantity = 0;
+				System.out.print("Product SN: ");
+				sequenceNumberOfProduct = scanner.nextInt();
+				if (!(sequenceNumberOfProduct <= item.noOfProductInList())) {
+					System.out.println("Sorry Now we have only "
+							+ item.noOfProductInList() + " Products");
+				} else if (!(sequenceNumberOfProduct > indexOfFirstProducts)) {
+					System.out.println("Sorry Prduct Sequence Starts From 1");
+				} else {
+					System.out.print("Quantitiy: ");
+					quantityOfProduct = scanner.nextInt();
+					if (!(quantityOfProduct > minProductQuantity)) {
+						System.out
+								.println("Sorry you need to buy one or more product");
+					} else if (!(quantityOfProduct < maxProductQuantity)) {
+						System.out
+								.println("Sorry at a time you can buy 100 or less products");
+					} else {
+						item.AddInCart(sequenceNumberOfProduct,
+								quantityOfProduct);
+					}
+				}
+				break;
+
+			// Case 2 to remove Product from the List
+			case 2:
+				if (item.isListEmpty() == true)
+					System.out.println("Sorry Your Bucket Is Empty");
+				else {
+					int sequenceNumberOfRemovableProduct;
+					item.displayFinalBucketProducts();
+					System.out
+							.print("Enter Product Sequence Number which you want to remove: ");
+					sequenceNumberOfRemovableProduct = scanner.nextInt();
+					if (sequenceNumberOfRemovableProduct > item.sizeOfList()) {
+						System.out.print("Sorry Product Sequence "
+								+ sequenceNumberOfRemovableProduct
+								+ " Doesn't Exist");
+					} else {
+						item.removeFromList(sequenceNumberOfRemovableProduct);
+					}
+				}
+				break;
+
+			// Case 3 to Show The Product List
+			case 3:
+				if (item.isListEmpty() == true)
+					System.out.println("Sorry Your Bucket Is Empty");
+				else {
+					item.displayFinalBucketProducts();
+				}
+				break;
+
+			// Case 4 to Place Order And Exit
+			case 4:
+				if (item.isListEmpty() == true) {
+					System.out.println("Your Bucket is Empty!");
+
+					System.out.println("DO you want to buy Product ?");
+					System.out.println("Press 1 for Yes and 2 for No");
+					System.out.println("Option :  ");
+					int wantToBuyMore = scanner.nextInt();
+					int yes = 1, no = 2;
+					if (wantToBuyMore == yes) {
+						System.out.println("Okay");
+					} else if (wantToBuyMore == no) {
+						System.out
+								.println("*********************************************************");
+						System.out
+								.println("\tWe hope that next time you will Buy");
+						System.out
+								.println("\t************ Thank You ************");
+						System.exit(0);
+					} else {
+						System.out.println("Sorry We Didn't Understand ");
+					}
+				} else {
+					item.orderPlace();
+					System.out.println("DO you want to buy more Product ?");
+					System.out.println("Press 1 for Yes and 2 for No");
+					System.out.println("Option :  ");
+					int wantToBuyMore = scanner.nextInt();
+					int yes = 1, no = 2;
+					if (wantToBuyMore == yes) {
+						System.out.println("Okay");
+					} else if (wantToBuyMore == no) {
+						System.out
+								.println("*********************************************************");
+						System.out
+								.println("\tYour Order is successfully Placed");
+						System.out
+								.println("\t*********** Thank You ***********");
+						System.exit(0);
+					} else {
+						System.out.println("Sorry We Didn't Understand ");
+					}
+				}
+
+				break;
+
+			default:
+				System.out
+						.println("Sorry we coudn't find this option\nPlease Try Again");
+			}
+		}
+	}
+}
